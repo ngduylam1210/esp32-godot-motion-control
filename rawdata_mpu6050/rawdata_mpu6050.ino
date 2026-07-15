64a700d646bbb6cf7ec695a464970db4abd1b86d
elayem_ng <ngduylam1210@gmail.com>
2026-07-15 14:44:50 +0700

    Update rawdata_mpu6050.ino

--- rawdata_mpu6050/rawdata_mpu6050.ino
+++ rawdata_mpu6050/rawdata_mpu6050.ino
@@ -6,9 +6,9 @@ void setup() {
   Serial.begin(115200);
   Wire.begin(21, 22);
 
-  Wire.beginTransmission(MPU); // bắt đầu nói chuyện với thanh ghi có địa chỉ 0x68
-  Wire.write(0x6B); // địa chỉ mặc định của MPU6050
-  Wire.write(0); // để tắt chế bit Sleep mặc định của MPU6050
+  Wire.beginTransmission(MPU); 
+  Wire.write(0x6B); 
+  Wire.write(0); 
   Wire.endTransmission();
 
   delay(1000);
@@ -17,22 +17,20 @@ void setup() {
 
 void loop() {
   Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // thanh ghi chứa 8 bit cao của trục X gia tốc
-  Wire.endTransmission(false); // Repeat Start, ngăn các thiết bị khác chen ngang quá trình đọc
-  Wire.requestFrom(MPU, 14, true); // đẩy ra 14 byte dữ liệu liên tiếp
+  Wire.write(0x3B); 
+  Wire.endTransmission(false); 
+  Wire.requestFrom(MPU, 14, true); 
 
-  // Bước 1: Đọc và ép về kiểu số nguyên 16-bit có dấu
   int16_t ax_raw = (Wire.read() << 8 | Wire.read());
   int16_t ay_raw = (Wire.read() << 8 | Wire.read());
   int16_t az_raw = (Wire.read() << 8 | Wire.read());
 
-  Wire.read(); Wire.read(); // Bỏ qua nhiệt độ
+  Wire.read(); Wire.read(); 
 
   int16_t gx_raw = (Wire.read() << 8 | Wire.read());
   int16_t gy_raw = (Wire.read() << 8 | Wire.read());
   int16_t gz_raw = (Wire.read() << 8 | Wire.read());
 
-  // Bước 2: Chia cho hệ số để ra giá trị thực (float)
   float ax = ax_raw / 16384.0;
   float ay = ay_raw / 16384.0;
   float az = az_raw / 16384.0;
